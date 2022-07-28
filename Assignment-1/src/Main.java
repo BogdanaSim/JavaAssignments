ba285c8711235a21b6033084be7c9e5b7fcbd02a
BogdanaSim <simionicabogdana@yahoo.com>
2022-07-28 12:29:21 +0300

    Added Assignment-1

---
+++ Assignment-1/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //1.
+        System.out.println("Hello\nBogdana\n");
+
+        //2.
+        System.out.println("12.5+23.9="+(12.5+23.9));
+
+        //3.
+        System.out.println("29/6="+((float)29/6));
+
+        //4.a.
+        System.out.println("-5+8*6="+(-5+8*6));
+
+        //4.b.
+        System.out.println("(55+9)%9="+((55+9)%9));
+
+        //4.c.
+        System.out.println("20+(-3)*5/8="+(20+(float)-3*5/8));
+
+        //4.d.
+        System.out.println("5+15/3*2-8%3="+(5+(float)15/3*2-8%3));
+    }
+}
